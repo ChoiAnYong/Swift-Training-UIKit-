57fa45eacb7067bb412b372cc83912fba2aec743
ChoiAnYong <dksdyd78@naver.com>
2023-11-01 22:21:49 +0900

    add

--- MyUIKit/MyTableViewController.swift
+++ MyUIKit/MyTableViewController.swift
@@ -17,18 +17,22 @@ class MyTableViewController: UIViewController {
         view.backgroundColor = .brown
         myTableView.backgroundColor = .green
         myTableView.delegate = self
-        myTableView.dataSource = self
+        myTableView.dataSource = self //이것 또한 delegate 패턴이다.
     }
 }
 
-extension MyTableViewController: UITableViewDelegate,
-    UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension MyTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    //정의만 해도 호출 없이도 실행된다
+    func numberOfSections(in tableView: UITableView) -> Int { //섹션이 몇 개인지
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //한 섹션에 몇개의 row가 들어가는지
         return cellData.count//cell의 개수를 리턴해준다.
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {//하나의 셀을 그릴 때 어떤 셀이 그려져야 할지
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         cell.textLabel?.text = cellData[indexPath.row] //cell를 하나 만들고 설정한다.
         return cell//해당 cell을 린턴해준다.
--- MyUIKit/MyViewController.swift
+++ MyUIKit/MyViewController.swift
@@ -6,21 +6,39 @@
 //
 
 import UIKit
+ 
+
+protocol AdminDelegate { //위임이 되면 해야할 것
+    func doTask()
+}
+
 
 class MyViewController: UIViewController {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var helloLabel: UILabel!
+    var admin: Admin?//관리자를 만든다.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        admin = Admin(delegate: self)//관리자를 만들고 위임을 self(MyViewController)에 했다
     }
     
     @IBAction func didTabButton(_ sender: Any) {
         if let name = nameTextField.text{
             helloLabel.text = "Hello \(name)!"
         }
+        admin?.delegate.doTask()
     }
+}
+
+extension MyViewController: AdminDelegate {//MyViewController가 위임을 받았다.
+    func doTask() {//위임을 받으면 할 것을 정의
+        print("저 지금 일 잘하고 있습니다!")
+    }
+}
+
+struct Admin { //Admin(관리자)이 위임을 한다.
+    var delegate: AdminDelegate//위임 받으면 해야할 일
 }
